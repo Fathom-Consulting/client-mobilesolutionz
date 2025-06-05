1dbea1cf6c1d2cc6441976b3194c53a49ee7e2e7
Ryan Wittmers <rwittmers19@georgefox.edu>
2025-06-04 18:20:05 -0700

    Changed landing page video

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,22 +1,29 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch('https://api.pexels.com/videos/videos/11641605', {
-      headers: {
-        'Authorization': process.env.PEXELS_API_KEY || '',
-        'Access-Control-Allow-Origin': '*',
-      }
-    });
+    const response = await fetch(
+      "https://api.pexels.com/videos/videos/13643113",
+      {
+        headers: {
+          Authorization: "process.env.PEXELS_API_KEY" || "",
+        },
+      },
+    );
     const data = await response.json();
     interface VideoFile {
       quality: string;
       link: string;
     }
-    const videoFile = data.video_files.find((file: VideoFile) => file.quality === 'hd');
-    return NextResponse.json({ videoUrl: videoFile?.link || '' });
+    const videoFile = data.video_files.find(
+      (file: VideoFile) => file.quality === "hd",
+    );
+    return NextResponse.json({ videoUrl: videoFile?.link || "" });
   } catch (error) {
-    console.error('Pexels API Fetch Error:', error);
-    return NextResponse.json({ error: 'Failed to fetch video' }, { status: 500 });
+    console.error("Pexels API Fetch Error:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch video" },
+      { status: 500 },
+    );
   }
 }
